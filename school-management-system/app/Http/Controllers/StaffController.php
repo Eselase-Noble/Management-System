607a4e52a98e7251b60965be183bc45d9dad67df
NOBLE ESELASE VULLEY <nevulley@st.ug.edu.gh>
2024-04-24 04:13:17 +0000

    Various Entities and Controllers created

---
+++ school-management-system/app/Http/Controllers/StaffController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Staff;
+use Illuminate\Http\Request;
+
+
+class StaffController extends Controller
+{
+    //selecy * from staff table
+
+    public function getAllStaffs(){
+        $staff =  Staff::all();
+        return response()->json($staff);
+    }
+
+    //Select * from staff table where id = $id
+    public function getStaffByID($id){
+        $staff = Staff::find($id);
+        if(!$staff){
+            return response()->json(['error'=> 'Staff not found'], 404);
+        }
+
+        return response()->json($staff);
+    }
+
+}
